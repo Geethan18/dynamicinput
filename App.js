fb7abefe8009d3598a938501faafe5aa91807d12
Geethan18 <100898474+Geethan18@users.noreply.github.com>
2022-11-25 16:29:54 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import RegisterForm from './ca1';
+function App(){
+    return(
+        <div className='App'>
+            <RegisterForm/>
+        </div>
+    )
+}
+
+export default App;
